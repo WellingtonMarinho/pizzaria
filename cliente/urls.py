632a2f8f9c5b9cdf6e03e6bae9450dd4104fe19c
Wellington <>
2021-02-17 22:44:47 -0300

    Correction of urls 'hífen'.

--- cliente/urls.py
+++ cliente/urls.py
@@ -4,8 +4,8 @@ from .views import ClienteListView, ClienteCreateView, ClienteUpdateView, Client
 
 urlpatterns = [
     path('clientes/', ClienteListView.as_view(), name='clientes'),
-    path('cliente/crie/', ClienteCreateView.as_view(), name='cliente_cria'),
-    path('cliente/edit/<int:cliente_pk>', ClienteUpdateView.as_view(), name='cliente_edit'),
-    path('cliente/delete/<int:cliente_pk>', ClienteDeleteView.as_view(), name='cliente_delete'),
+    path('cliente/crie/', ClienteCreateView.as_view(), name='cliente-cria'),
+    path('cliente/edit/<int:cliente_pk>', ClienteUpdateView.as_view(), name='cliente-edit'),
+    path('cliente/delete/<int:cliente_pk>', ClienteDeleteView.as_view(), name='cliente-delete'),
 
 ]
--- produto/urls.py
+++ produto/urls.py
@@ -4,14 +4,14 @@ from .views import ProdutoListView, ProdutoCreateView, ProdutoUpdateView, Produt
 
 
 urlpatterns = [
-    path('produtos/', ProdutoListView.as_view(), name='lista_produtos'),
-    path('produto/crie/', ProdutoCreateView.as_view(), name='produtos_create'),
-    path('produto/edit/<int:produto_pk>', ProdutoUpdateView.as_view(), name='produtos_edit'),
-    path('produto/delete/<int:produto_pk>', ProdutoDeleteView.as_view(), name='produtos_delete'),
+    path('produtos/', ProdutoListView.as_view(), name='lista-produtos'),
+    path('produto/crie/', ProdutoCreateView.as_view(), name='produtos-create'),
+    path('produto/edit/<int:produto_pk>', ProdutoUpdateView.as_view(), name='produtos-edit'),
+    path('produto/delete/<int:produto_pk>', ProdutoDeleteView.as_view(), name='produtos-delete'),
     # path('<slug>', ProdutoDetailView.as_view(), name='produto'),
 
     path('categorias/', CategoriaListView.as_view(), name='categorias'),
-    path('categoria/crie/', CategoriaCreateView.as_view(), name='categoria_create'),
-    path('categoria/edit/<int:categoria_pk>', CategoriaUpdateView.as_view(), name='categoria_edit'),
-    path('categoria/delete//<int:categoria_pk>', CategoriaDeleteView.as_view(), name='categoria_delete'),
+    path('categoria/crie/', CategoriaCreateView.as_view(), name='categoria-create'),
+    path('categoria/edit/<int:categoria_pk>', CategoriaUpdateView.as_view(), name='categoria-edit'),
+    path('categoria/delete/<int:categoria_pk>', CategoriaDeleteView.as_view(), name='categoria-delete'),
 ]
